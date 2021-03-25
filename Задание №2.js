11f9007c35d16d65d5fa98de2dcea25c72844ee9
Ivan Kranchev <81128131+flyerrmax@users.noreply.github.com>
2021-03-25 23:24:22 +0700

    Add files via upload

---
+++ Задание №2.js
@@ -0,0 +1,10 @@
+let lines = 4;
+let str = " ";
+let star = "**";
+  for(let i = 0; i < lines; i++){
+    if(i == 0){
+        console.log(" *")
+    }
+    str += star;
+    console.log(str+"*");
+}
